d5459b5dedfad0a6a24a2a60a7db5fbe111ce15e
sagar2395 <sagar.chhabra02@gmail.com>
2025-03-17 21:00:03 +0530

    Update kubernetes application

--- scripts/k8s-update.ps1
+++ scripts/k8s-update.ps1
@@ -1,19 +1,20 @@
 
 
-$env = "dev"
+$env = "dev" # dev, prod
 
 # Builds and pushes the images
 docker-compose -f .\docker-compose.base.yml -f .\docker-compose.$env.yml build
 
+# Pushes the images to the registry
 if($env -eq "prod") {
    docker-compose -f .\docker-compose.base.yml -f .\docker-compose.$env.yml push
 }
-# Pushes the images to the registry
 
 
 # Updates the k8s deployment
-kubectl delete -k .\manifests\overlays\$env
-kubectl apply -k .\manifests\overlays\$env
+kubectl delete -k .\manifests\overlays\$env # Deletes the current deployment
+kubectl apply -k .\manifests\overlays\$env # Updates the deployment
+kubectl apply -f .\maniefests\common # Updates common resources
 
 # For minikube
-minikube tunnel 
+minikube tunnel # Exposes the service to the host machine
