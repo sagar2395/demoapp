086dd08dfdfec3d8c538a245809a2688e470c627
sagar2395 <sagar.chhabra02@gmail.com>
2025-03-17 21:35:04 +0530

    Setting up terraform

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "demoapp_rg" {
+   name     = "demoapp-rg"
+   location = "East US" # Change to your desired Azure region
+}
--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -8,3 +8,20 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  features {
+
+  }
+
+  subscription_id = "88550cab-be9d-470e-bb27-7fc3ff569ee3"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "sagarchhabra-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "cloudinsightssa"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
